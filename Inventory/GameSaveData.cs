a1817bbed7cb877b475bf736fdfb7d626300b0db
flawlesshappiness <bkk@arabesque.dk>
2024-06-15 14:11:12 +0200

    Added tutorial text

--- Inventory/GameSaveData.cs
+++ Inventory/GameSaveData.cs
@@ -2,5 +2,5 @@ using System.Collections.Generic;
 
 public partial class GameSaveData
 {
-    public List<InventoryItem> InventoryItems { get; set; }
+    public List<InventoryItem> InventoryItems { get; set; } = new();
 }

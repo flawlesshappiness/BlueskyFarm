6bb8c11e5f1520fb3581df4e97f3bc74d941a1d3
flawlesshappiness <bkk@arabesque.dk>
2024-08-26 23:35:04 +0200

    Added EnvironmentFadeArea

---
+++ Environment/EnvironmentFadeArea.cs
@@ -0,0 +1,77 @@
+using Godot;
+
+public partial class EnvironmentFadeArea : Node3DScript
+{
+    [Export]
+    public Environment EnvStart;
+
+    [Export]
+    public Environment EnvEnd;
+
+    [NodeType]
+    public Area3D Area;
+
+    [NodeName]
+    public Node3D Start;
+
+    [NodeName]
+    public Node3D End;
+
+    private bool _tracking;
+    private EnvironmentLerp _lerp;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        Area.BodyEntered += v => CallDeferred(nameof(BodyEntered), v);
+        Area.BodyExited += v => CallDeferred(nameof(BodyExited), v);
+    }
+
+    private void BodyEntered(GodotObject obj)
+    {
+        var player = obj as FirstPersonController;
+        if (player == null) return;
+
+        _tracking = true;
+
+        var scene = Scene.Current as GameScene;
+        if (scene == null) return;
+
+        _lerp = EnvStart.CreateLerp(EnvEnd);
+        _lerp.UpdateLerp(GetLerpValue());
+        scene.WorldEnvironment.Environment = _lerp.Result;
+    }
+
+    private void BodyExited(GodotObject obj)
+    {
+        var player = obj as FirstPersonController;
+        if (player == null) return;
+
+        _tracking = false;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        Process_Tracking();
+    }
+
+    private void Process_Tracking()
+    {
+        if (!_tracking) return;
+        var t = GetLerpValue();
+        _lerp.UpdateLerp(t);
+    }
+
+    private float GetLerpValue()
+    {
+        var p = FirstPersonController.Instance.GlobalPosition;
+        var a = p - Start.GlobalPosition;
+        var b = End.GlobalPosition - Start.GlobalPosition;
+        var angle = a.AngleTo(b);
+        var cos = Mathf.Cos(angle);
+        var v = (a.Length() * cos) / b.Length();
+        return Mathf.Clamp(v, 0, 1);
+    }
+}

34b1cf4eb202ef6cafd87c68648b1a1140605fe3
flawlesshappiness <bkk@arabesque.dk>
2024-12-22 19:30:04 +0100

    Updated root mimic animations and AI

--- Enemy/RootMimic/RootMimicEnemy.cs
+++ Enemy/RootMimic/RootMimicEnemy.cs
@@ -1,72 +1,21 @@
+using FlawLizArt.Animation.StateMachine;
 using Godot;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 
 public partial class RootMimicEnemy : NavEnemy
 {
+    [NodeType]
+    public AnimationStateMachine AnimationStateMachine;
+
+    [NodeType]
+    public AnimationPlayer AnimationPlayer;
+
     [NodeName]
     public AudioStreamPlayer3D SfxThreat;
 
     [NodeName]
     public AudioStreamPlayer3D SfxGrowl;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg1_R;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg2_R;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg3_R;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg1_L;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg2_L;
-
-    [NodeName]
-    public SkeletonIK3D ik_leg3_L;
-
-    [NodeName]
-    public SkeletonIK3D ik_neck;
-
-    [NodeName]
-    public Node3D Leg1_R;
-
-    [NodeName]
-    public Node3D Leg2_R;
-
-    [NodeName]
-    public Node3D Leg3_R;
-
-    [NodeName]
-    public Node3D Leg1_L;
-
-    [NodeName]
-    public Node3D Leg2_L;
-
-    [NodeName]
-    public Node3D Leg3_L;
-
-    [NodeName]
-    public Node3D Neck;
-
-    [NodeName]
-    public Node3D Neck_ThreatPosition;
-
-    [NodeName]
-    public Node3D Neck_WalkPosition;
-
-    [NodeName]
-    public Node3D Neck_WaitPosition;
-
-    [NodeName]
-    public Node3D Leg3_L_ThreatPosition;
-
-    [NodeName]
-    public Node3D Leg3_R_ThreatPosition;
 
     private enum State
     {
@@ -76,72 +25,39 @@ public partial class RootMimicEnemy : NavEnemy
 
     private State _state;
     private float _time_step_sfx;
-    private float _time_move_pause;
-    private float _time_move_pause_check;
     private bool _spawned;
-    private bool _ignore_move_pause;
     private bool _state_change_disabled;
     private RandomNumberGenerator _rng = new RandomNumberGenerator();
     private BasementRoomElement _current_room;
 
-    private List<Limb> _limbs = new();
+    private AnimationState _anim_walk;
+    private AnimationState _anim_threat;
+    private AnimationState _anim_waiting;
+    private AnimationState _anim_attack;
+
+    private BoolParameter _param_moving;
+    private BoolParameter _param_threat;
+    private TriggerParameter _param_attack;
 
     private const float CHANCE_THREAT = 0.5f;
-    private const float CHANCE_WANDER = 0.5f;
     private const float DIST_WAIT_NEAR = 24;
     private const float DIST_WAIT_FAR = 28;
-    private const float DIST_THREAT = 8;
+    private const float DIST_THREAT = 6;
     private const float DIST_THREAT_CLOSE = 4;
     private const float DIST_THREAT_ATTACK = 2;
-
-    private class Limb
-    {
-        public SkeletonIK3D IK { get; }
-        public Node3D Target { get; }
-        public Vector3 LocalPosition { get; }
-        public Vector3 LocalRotation { get; }
-        public Vector3 TargetPosition { get; set; }
-        public float MaxDistance { get; set; }
-        public float TargetSpeed { get; set; }
-
-        private float _init_max_distance;
-        private RandomNumberGenerator _rng = new RandomNumberGenerator();
-
-        public Limb(SkeletonIK3D ik, Node3D target)
-        {
-            IK = ik;
-            Target = target;
-            LocalPosition = Target.Position;
-            LocalRotation = Target.Rotation;
-            MaxDistance = 1f;
-            TargetSpeed = 15f;
-
-            _init_max_distance = MaxDistance;
-        }
-
-        public void UpdateTargetPosition()
-        {
-            Target.GlobalPosition = Target.GlobalPosition.Lerp(TargetPosition, TargetSpeed * GameTime.DeltaTime);
-        }
-
-        public void RandomizeMaxDistance()
-        {
-            var range = 0.1f;
-            MaxDistance = _rng.RandfRange(_init_max_distance - range, _init_max_distance + range);
-        }
-    }
 
     public override void _Ready()
     {
         base._Ready();
 
         RegisterDebugActions();
-        InitializeLimbs();
     }
 
     protected override void Initialize()
     {
         base.Initialize();
+
+        InitializeAnimations();
 
         if (IsDebug)
         {
@@ -151,6 +67,43 @@ public partial class RootMimicEnemy : NavEnemy
         {
             Spawn();
         }
+    }
+
+    private void InitializeAnimations()
+    {
+        _anim_waiting = AnimationStateMachine.CreateAnimation("Armature|waiting", false);
+        _anim_walk = AnimationStateMachine.CreateAnimation("Armature|walk", true);
+        _anim_threat = AnimationStateMachine.CreateAnimation("Armature|threat", true);
+        _anim_attack = AnimationStateMachine.CreateAnimation("Armature|attack", false);
+
+        _param_moving = AnimationStateMachine.CreateParameter("moving", false);
+        _param_threat = AnimationStateMachine.CreateParameter("threat", false);
+        _param_attack = AnimationStateMachine.CreateParameter("attack");
+
+        AnimationStateMachine.Connect(_anim_waiting.Node, _anim_walk.Node, _param_moving.WhenTrue());
+        AnimationStateMachine.Connect(_anim_walk.Node, _anim_waiting.Node, _param_moving.WhenFalse());
+
+        AnimationStateMachine.Connect(_anim_walk.Node, _anim_threat.Node, _param_threat.WhenTrue());
+        AnimationStateMachine.Connect(_anim_waiting.Node, _anim_threat.Node, _param_threat.WhenTrue());
+        AnimationStateMachine.Connect(_anim_threat.Node, _anim_walk.Node, _param_threat.WhenFalse());
+
+        AnimationStateMachine.Connect(_anim_waiting.Node, _anim_attack.Node, _param_attack.WhenTriggered());
+        AnimationStateMachine.Connect(_anim_walk.Node, _anim_attack.Node, _param_attack.WhenTriggered());
+        AnimationStateMachine.Connect(_anim_threat.Node, _anim_attack.Node, _param_attack.WhenTriggered());
+
+        AnimationStateMachine.Initialize(AnimationPlayer);
+        AnimationStateMachine.Start(_anim_waiting.Node);
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        ProcessAnimations();
+    }
+
+    private void ProcessAnimations()
+    {
+        _param_moving.Set(!Agent.IsNavigationFinished());
     }
 
     private void RegisterDebugActions()
@@ -192,33 +145,6 @@ public partial class RootMimicEnemy : NavEnemy
             Text = "Set state (Threat)",
             Action = _ => SetState(State.Threat)
         });
-    }
-
-    private void InitializeLimbs()
-    {
-        _limbs = new List<Limb>
-        {
-            new Limb(ik_leg1_L, Leg1_L),
-            new Limb(ik_leg2_L, Leg2_L),
-            new Limb(ik_leg3_L, Leg3_L),
-            new Limb(ik_leg1_R, Leg1_R),
-            new Limb(ik_leg2_R, Leg2_R),
-            new Limb(ik_leg3_R, Leg3_R),
-        };
-
-        foreach (var limb in _limbs)
-        {
-            limb.IK.Start();
-            limb.Target.SetParent(GetParent());
-        }
-
-        ik_neck.Start();
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-        Process_Limbs();
     }
 
     private void Spawn()
@@ -264,53 +190,30 @@ public partial class RootMimicEnemy : NavEnemy
 
     private IEnumerator StateCr_Wander()
     {
-        _ignore_move_pause = false;
-
-        var move_to_room_center = false;
         var time_wait = GameTime.Time;
         while (true)
         {
-            if (move_to_room_center)
-            {
-                if (DistanceToPlayer < DIST_WAIT_NEAR * 0.75f || Agent.IsNavigationFinished())
-                {
-                    SetState(State.Waiting);
-                    break;
-                }
-
-                yield return null;
-                continue;
-            }
-
             if (DistanceToPlayer < DIST_WAIT_NEAR)
             {
-                var closest_room = GetClosestRoomElements(AreaNames.Basement)
-                    .Take(2)
-                    .OrderByDescending(x => x.Room.GlobalPosition.DistanceTo(PlayerPosition)) // Get room furthest from the player
-                    .FirstOrDefault();
-
-                if (GlobalPosition.DistanceTo(closest_room.Room.GlobalPosition) > BasementRoom.ROOM_SIZE * 0.5f)
-                {
-                    _current_room = closest_room;
-                    Agent.TargetPosition = GetRandomPositionInRoom(_current_room.Room);
-                    move_to_room_center = true;
-                    yield return null;
-                    continue;
-                }
-
                 SetState(State.Waiting);
                 break;
             }
 
             if (GameTime.Time > time_wait)
             {
-                if (_rng.RandfRange(0, 1) < CHANCE_WANDER)
+                var next_room = BasementController.Instance.CurrentBasement.Grid
+                        .GetNeighbours(_current_room.Coordinates) // Get neighbours
+                        .OrderBy(x => x.Room.GlobalPosition.DistanceTo(Player.Instance.GlobalPosition)) // that is closest to player
+                        .Where(x => x.Room.GlobalPosition.DistanceTo(Player.Instance.GlobalPosition) > BasementRoom.ROOM_SIZE * 0.5f) // that player is not in
+                        .FirstOrDefault();
+
+                if (next_room != null)
                 {
-                    _current_room = GetClosestRoomElementToPlayer(AreaNames.Basement);
+                    _current_room = next_room;
                     Agent.TargetPosition = GetRandomPositionInRoom(_current_room.Room);
                 }
 
-                time_wait = GameTime.Time + _rng.RandfRange(2, 10);
+                time_wait = GameTime.Time + _rng.RandfRange(15, 30);
             }
 
             yield return null;
@@ -320,7 +223,6 @@ public partial class RootMimicEnemy : NavEnemy
     private IEnumerator StateCr_Waiting()
     {
         StopNavigation();
-        AnimatePose_Wait();
 
         while (true)
         {
@@ -358,8 +260,8 @@ public partial class RootMimicEnemy : NavEnemy
     private IEnumerator StateCr_Threat()
     {
         StopNavigation();
-        AnimatePose_Threat();
 
+        _param_threat.Set(true);
         SfxGrowl.Play();
 
         var duration = 4f;
@@ -379,19 +281,19 @@ public partial class RootMimicEnemy : NavEnemy
         if (DistanceToPlayer < DIST_THREAT_CLOSE)
         {
             Player.StopLookingAt();
+            _param_threat.Set(false);
             SetState(State.Attacking);
         }
         else
         {
             Player.StopLookingAt();
-            AnimatePose_Walk();
+            _param_threat.Set(false);
             SetState(State.Fleeing);
         }
     }
 
     private IEnumerator StateCr_Fleeing()
     {
-        _ignore_move_pause = true;
         _current_room = GetFurthestRoomElementToPlayer(AreaNames.Basement);
         Agent.TargetPosition = GetRandomPositionInRoom(_current_room.Room);
         SfxThreat.Play();
@@ -409,12 +311,11 @@ public partial class RootMimicEnemy : NavEnemy
 
     private IEnumerator StateCr_Attacking()
     {
-        _ignore_move_pause = true;
-
         var id_lock = nameof(RootMimicEnemy) + GetInstanceId();
         Player.MovementLock.AddLock(id_lock);
 
         SfxGrowl.Play();
+        _param_attack.Trigger();
 
         while (DistanceToPlayer > DIST_THREAT_ATTACK)
         {
@@ -443,116 +344,9 @@ public partial class RootMimicEnemy : NavEnemy
         }
     }
 
-    protected override void OnVelocityComputed(Vector3 v)
+    public void PlayStepSfx()
     {
-        if (GameTime.Time > _time_move_pause_check)
-        {
-            _time_move_pause_check = GameTime.Time + _rng.RandfRange(2f, 4f);
-            _time_move_pause = GameTime.Time + _rng.RandfRange(0.5f, 1.5f);
-        }
-
-        if (!_ignore_move_pause && GameTime.Time < _time_move_pause) return;
-
-        base.OnVelocityComputed(v);
-    }
-
-    private void Process_Limbs()
-    {
-        foreach (var limb in _limbs)
-        {
-            if (IsThreatLimb(limb))
-            {
-                Process_Limb_Threat(limb);
-            }
-            else
-            {
-                Process_Limb_Walk(limb);
-            }
-        }
-
-        bool IsThreatLimb(Limb limb) => (_state == State.Threat || _state == State.Attacking) && (limb.Target == Leg3_L || limb.Target == Leg3_R);
-    }
-
-    private void Process_Limb_Walk(Limb limb)
-    {
-        var new_position = GlobalPosition + GlobalBasis * limb.LocalPosition;
-        var distance = limb.Target.GlobalPosition.DistanceTo(new_position);
-
-        if (distance > limb.MaxDistance)
-        {
-            limb.TargetPosition = new_position + (GlobalBasis * Vector3.Back * limb.MaxDistance * 0.5f);
-            limb.Target.GlobalRotation = GlobalRotation + GlobalBasis * limb.LocalRotation;
-            limb.RandomizeMaxDistance();
-
-            PlayStepSfx(limb.TargetPosition);
-        }
-
-        limb.UpdateTargetPosition();
-    }
-
-    private void Process_Limb_Threat(Limb limb)
-    {
-        var L = limb.Target == Leg3_L;
-        var new_position = L ? Leg3_L_ThreatPosition : Leg3_R_ThreatPosition;
-        limb.TargetPosition = new_position.GlobalPosition;
-        limb.Target.GlobalRotation = new_position.GlobalRotation;
-        limb.UpdateTargetPosition();
-    }
-
-    private void PlayStepSfx(Vector3 position)
-    {
-        if (GameTime.Time < _time_step_sfx) return;
-        _time_step_sfx = GameTime.Time + 0.1f;
-
-        SoundController.Instance.Play("sfx_root_mimic_walk", position);
-    }
-
-    private void AnimatePose_Threat()
-    {
-        this.StartCoroutine(Cr, "pose");
-        IEnumerator Cr()
-        {
-            var curve = Curves.EaseOutQuad;
-            var start_position = Neck.Position;
-            var start_rotation = Neck.Rotation;
-            yield return LerpEnumerator.Lerp01(0.25f, f =>
-            {
-                Neck.Position = start_position.Lerp(Neck_ThreatPosition.Position, curve.Evaluate(f));
-                Neck.Rotation = start_rotation.Lerp(Neck_ThreatPosition.Rotation, curve.Evaluate(f));
-            });
-        }
-    }
-
-    private void AnimatePose_Walk()
-    {
-        this.StartCoroutine(Cr, "pose");
-        IEnumerator Cr()
-        {
-            var curve = Curves.EaseOutQuad;
-            var start_position = Neck.Position;
-            var start_rotation = Neck.Rotation;
-            yield return LerpEnumerator.Lerp01(0.25f, f =>
-            {
-                Neck.Position = start_position.Lerp(Neck_WalkPosition.Position, curve.Evaluate(f));
-                Neck.Rotation = start_rotation.Lerp(Neck_WalkPosition.Rotation, curve.Evaluate(f));
-            });
-        }
-    }
-
-    private void AnimatePose_Wait()
-    {
-        this.StartCoroutine(Cr, "pose");
-        IEnumerator Cr()
-        {
-            var curve = Curves.EaseOutQuad;
-            var start_position = Neck.Position;
-            var start_rotation = Neck.Rotation;
-            yield return LerpEnumerator.Lerp01(0.25f, f =>
-            {
-                Neck.Position = start_position.Lerp(Neck_WaitPosition.Position, curve.Evaluate(f));
-                Neck.Rotation = start_rotation.Lerp(Neck_WaitPosition.Rotation, curve.Evaluate(f));
-            });
-        }
+        SoundController.Instance.Play("sfx_root_mimic_walk", GlobalPosition);
     }
 
     private Vector3 GetRandomPositionInRoom(BasementRoom room)

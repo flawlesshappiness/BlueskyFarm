04441fa9c3be7cc02a4ef1c1bd6f063f9cd74b01
flawlesshappiness <bkk@arabesque.dk>
2025-01-19 07:03:27 +0100

    Added unlockable shed

--- Farm/FarmShed.cs
+++ Farm/FarmShed.cs
@@ -1,9 +1,13 @@
 using Godot;
+using System.Collections;
 
 public partial class FarmShed : Node3D
 {
     [Export]
     public UnlockableGroup UnlockGroup;
+
+    [Export]
+    public ItemArea UnlockItemArea;
 
     private string DebugCategory = "SHED";
 
@@ -11,8 +15,21 @@ public partial class FarmShed : Node3D
     {
         base._Ready();
         RegisterDebugActions();
+        UnlockItemArea.OnItemEntered += ItemEntered;
 
-        UnlockGroup.SetNotUnlocked();
+        Load();
+    }
+
+    private void Load()
+    {
+        if (Data.Game.Flag_ShedRepaired)
+        {
+            UnlockGroup.SetUnlocked();
+        }
+        else
+        {
+            UnlockGroup.SetNotUnlocked();
+        }
     }
 
     public override void _ExitTree()
@@ -38,5 +55,27 @@ public partial class FarmShed : Node3D
             Text = "Lock Shed",
             Action = v => UnlockGroup.SetNotUnlocked()
         });
+
+        Debug.RegisterAction(new DebugAction
+        {
+            Id = DebugCategory,
+            Category = DebugCategory,
+            Text = "Clear data",
+            Action = v => Data.Game.Flag_ShedRepaired = false
+        });
+    }
+
+    private void ItemEntered(Item item)
+    {
+        UnlockItemArea.Disable();
+        Data.Game.Flag_ShedRepaired = true;
+
+        Coroutine.Start(Cr);
+        IEnumerator Cr()
+        {
+            SoundController.Instance.Play("sfx_throw_light", item.GlobalPosition);
+            yield return item.AnimateDisappearAndQueueFree();
+            yield return UnlockGroup.AnimateUnlock();
+        }
     }
 }

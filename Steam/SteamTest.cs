cf22010ce4784d6aadec5e7af11811957d5c2d28
flawlesshappiness <bkk@arabesque.dk>
2025-04-12 04:58:07 +0200

    Steam initialization is working

---
+++ Steam/SteamTest.cs
@@ -0,0 +1,28 @@
+using Godot;
+using GodotSteam;
+
+public partial class SteamTest : Node
+{
+    private const uint AppId = 480;
+
+    public override void _EnterTree()
+    {
+        OS.SetEnvironment("SteamAppId", AppId.ToString());
+        OS.SetEnvironment("SteamGameId", AppId.ToString());
+    }
+
+    public override void _Ready()
+    {
+        var isSteamRunning = Steam.IsSteamRunning();
+        if (!isSteamRunning)
+        {
+            GD.Print("Steam is not running.");
+            return;
+        }
+
+        var steamId = Steam.GetSteamID();
+        var name = Steam.GetFriendPersonaName(steamId);
+
+        GD.Print("Your Steam Name: " + name);
+    }
+}

fc0d1cbdf7804b1de232aa1e62a977ce2a97644c
flawlesshappiness <bkk@arabesque.dk>
2025-07-21 03:31:45 +0200

    Brightness can now be adjusted and loaded on MainMenuScene

--- Scenes/MainMenuScene.cs
+++ Scenes/MainMenuScene.cs
@@ -6,6 +6,9 @@ public partial class MainMenuScene : Scene
     [NodeName]
     public Node3D CameraTarget;
 
+    [Export]
+    public WorldEnvironment WorldEnvironment;
+
     private MainMenuView _view;
 
     public override void _Ready()
@@ -15,6 +18,9 @@ public partial class MainMenuScene : Scene
         ScreenEffects.View.SetCameraTarget(CameraTarget);
 
         AmbienceController.Instance.StartAmbienceImmediate(AreaNames.Farm);
+
+        UpdateBrightness();
+        WorldEnvironment.Environment.AdjustmentEnabled = true;
 
         Coroutine.Start(Cr);
         IEnumerator Cr()
@@ -28,5 +34,24 @@ public partial class MainMenuScene : Scene
             _view = View.Get<MainMenuView>();
             _view.Show();
         }
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        OptionsController.Instance.OnBrightnessChanged += UpdateBrightness;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        OptionsController.Instance.OnBrightnessChanged -= UpdateBrightness;
+    }
+
+    private void UpdateBrightness()
+    {
+        if (!IsInstanceValid(WorldEnvironment)) return;
+
+        WorldEnvironment.Environment.AdjustmentBrightness = Data.Options.Brightness;
     }
 }
